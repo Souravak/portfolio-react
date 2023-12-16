ada63ceae261b02b6267fe15c9c6f803ee16bff9
souravak <souravak211@gmail.com>
2023-12-16 15:53:12 +0530

    version added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,8 @@ const Footer = () => {
   return (
     <footer className="footer">
         Copyright &#169; 2023 Sourav A K. All right reserved.
+        <br />
+        version : 1.0.8
     </footer>
   )
 }
